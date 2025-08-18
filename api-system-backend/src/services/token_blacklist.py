61e49d2df1faa5215b9b534c54aa9150f7566da0
Rox Ram <39720515+roxRam16@users.noreply.github.com>
2025-08-17 22:11:54 -0600

    Indexacion de gitignore

--- api-system-backend/src/services/token_blacklist.py
+++
@@ -1,8 +0,0 @@
-# src/services/token_blacklist.py
-revoked_tokens = set()
-
-def add_token_to_blacklist(token: str):
-    revoked_tokens.add(token)
-
-def is_token_revoked(token: str) -> bool:
-    return token in revoked_tokens
